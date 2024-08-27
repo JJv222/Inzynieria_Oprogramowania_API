ad033dd69bea26bdd22da253915ec47ee4190c61
JJv222 <kubcat828@gmail.com>
2024-08-27 18:38:20 +0200

    Add project files.

---
+++ Data_Models/User_Model.cs
@@ -0,0 +1,19 @@
+﻿namespace Inzynieria_oprogramowania_API.Data_Models
+{
+	abstract public class User_Model
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+		public string role { get; set; }
+		public DateTime createdAt { get; set; }
+		public DateTime updatedAt { get; set; }
+		public DateTime deletedAt { get; set; }
+
+
+		//FK
+		public int? postId { get; set; }
+		public 
+	}
+}
---
+++ Database/ProjectContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inzynieria_oprogramowania_API.Database
+{
+	public class ProjectContext : DbContext
+	{
+		protected readonly IConfiguration Configuration;
+		private static string Host = "my_host";
+		private static string Database = "my_db";
+		private static string UserName = "my_user";
+		private static string Password = "my_pw";
+
+		public ProjectContext(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+			=> optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+
+	}
+}
